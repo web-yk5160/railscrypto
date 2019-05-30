33088e42e61e2e61ad87b9ffc1030b8fe2ef3862
web-yk5160 <rubystudy0123@gmail.com>
2019-05-30 14:26:57 +0900

    Added Devise

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,33 @@
+class HomeController < ApplicationController
+  def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "XRP", "ADA", "XLM", "STEEM"]
+  end
+
+  def about
+  end
+
+  def lookup
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coins = JSON.parse(@response)
+
+    @symbol = params[:sym]
+
+    if @symbol
+      @symbol = @symbol.upcase
+    end
+
+    if @symbol == ""
+      @symbol = "コイン名を入力してください"
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/about'
+  get 'home/lookup'
+  post "home/lookup" => 'home/lookup'
+  root 'home#index'
+end
